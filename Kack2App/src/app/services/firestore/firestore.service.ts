ae45a1593c7fefd77bdb0838e6b797634884350c
AresianGTI <onobenik@freenet.de>
2021-03-22 15:56:06 +0100

    try to export firestore methods to firestore-serv

--- Kack2App/src/app/services/firestore/firestore.service.ts
+++ Kack2App/src/app/services/firestore/firestore.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
 import { GlobalstringsService } from '../globalstrings/globalstrings.service';
 
 @Injectable({
@@ -6,7 +8,28 @@ import { GlobalstringsService } from '../globalstrings/globalstrings.service';
 })
 export class FirestoreService {
 
-  constructor() { 
+  constructor(public afs: AngularFirestore) { 
+
+  }
+    getUserData(user: any, subscriptionList: Subscription[]) {
+      let userData;
+      //Check ob subscription schon vorhanden ist--> Wenn ja soll keine weitere Subscription erstellt werden
+      subscriptionList.push(
+        this.afs.collection("users").doc(`/${user.uid}`).valueChanges()
+        .subscribe(value =>
+          {
+            //Subscription muss stoppen
+            // this.userData = value;
+            userData = value;
+            console.log("RESULT",  value);
+            // for(let sub in subscriptionList){
+            //   console.log("ICh bin ein SUBMARINA", sub);
+            // }
+          })
+          
+      );
+      console.log("UserData: ", userData);
+      return userData;
 
   }
 }

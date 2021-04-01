c0b75917b3e374289f91be1315d2b26cdbbc0847
Marc <keilmannm@yahoo.de>
2021-04-01 20:42:07 +0200

    Create Map of Objects in Firestore

--- Kack2App/src/app/services/calendar/calendar.service.ts
+++ Kack2App/src/app/services/calendar/calendar.service.ts
@@ -10,6 +10,7 @@ import {
   isSameDay,
   isSameMonth,
   addHours,
+  eachWeekOfInterval,
 } from 'date-fns';
 import { Subject } from 'rxjs';
 import {
@@ -22,6 +23,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { EnumMeetingTypes } from '../enums/enums.service';
 import { FirestoreService } from '../firestore/firestore.service';
 import { AuthService } from 'src/app/core/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 const colors: any = {
   red: {
     primary: '#ad2121',
@@ -47,7 +49,8 @@ export class CalendarService {
   constructor(
     public dialog: MatDialog,
     public firestoreService: FirestoreService,
-    public authService: AuthService) { }
+    public authService: AuthService,
+    public afs: AngularFirestore) { }
   view: CalendarView = CalendarView.Month;
   CalendarView = CalendarView;
   viewDate: Date = new Date();
@@ -111,43 +114,87 @@ export class CalendarService {
     this.events.filter((event) => this.firestoreService.deleteDocument(event, "CalendarMeetings"));
     this.events = this.events.filter((event) => event !== eventToDelete);
   }
+  
 
-
+  stabil: any[] = []
+  item: any ={
+    ID: this.firestoreService.createID(),
+    // start: subDays(startOfDay(new Date(),1),
+    start: startOfDay(new Date()),
+    end: endOfDay(new Date()),
+    // end: addDays(new Date(),1),
+    title: "type",
+    color: colors.black,
+    allDay: true,
+    resizable: {
+      beforeStart: true,
+      afterEnd: true,
+    }
+  }
+  menmen: any = {
+    UID: "",
+    items:{
+    }
+  }
+ 
   testData() {
-    let menmen: any = {
-      UID: this.authService.userData?.ID,
+    
+
+      // this.getUserData(this.authService.userData).then((val) => {
+      //   if(val)
+      //   {
+          // console.log("Val ObjData: ", val);
+          // this.stabil.indexOf("")
+          let id = this.firestoreService.createID();
+            // this.stabil.push(menmen);
+            this.menmen.UID =this.authService.userData?.ID;
+            this.menmen.items["ItemID "+ id] = this.item;
+            console.log("stabil: ", this.menmen);
+          this.firestoreService.updateDocument("Test", this.menmen);
+      //   }
+      //   else
+      //   {
+      //     this.stabil = [menmen]
+      //     // this.stabil = [...this.stabil, menmen]
+      //     console.log("Else");
+      //     // this.firestoreService.updateDocument("Test", this.stabil);
+      //   }
+      // })
+   
+  }
+  gggg() {
+    let js = {
       item:{
-        ID: this.firestoreService.createID(),
-        // start: subDays(startOfDay(new Date(),1),
-        start: startOfDay(new Date()),
-        end: endOfDay(new Date()),
-        // end: addDays(new Date(),1),
-        title: "type",
-        color: colors.black,
-        allDay: true,
-        resizable: {
-          beforeStart: true,
-          afterEnd: true,
-        }
+        ff: 22,
+        sss: 1
       }
      
     }
-    console.log("MENMEN: ", menmen);
-    // this.firestoreService.updateDocument("Test", menmen);
-    let stabil  = [
-    menmen ]
-    stabil.push(menmen);
-    stabil = [...stabil, menmen]
-    console.log("Stabil: ", stabil);
-  
-      this.firestoreService.updateDocument("Test",   this.gggg(stabil));
-    return menmen;
+    return js
   }
-  gggg(stabil: Array<any>): Array<any>{
-    let js = {
-      ff:22,
-      sss:1
-    }
-     return stabil = [...stabil, js ]
+  dataMata() {
+    this.getUserData(this.authService.userData);
   }
+  getUserData = (user: any): Promise<any> => {
+    var docRef = this.afs.collection("Test").doc(`${user.ID}`);
+    return docRef.ref.get().then((doc) => {
+      console.log("Data des Kalenders:", doc.data());
+      console.log("UID:", user.ID);
+      return doc.data();
+    })
+  }
+  // getDocFromCollection(collection: string, data: any):Array<any> {
+  //   let fieldList: Array<{}> = [];
+  //   this.afs.collection("collection", ref => ref.where("UID", "==", data.UID) )
+  //     .get().toPromise().then(snapshot => {
+  //       snapshot.docs.forEach(doc => { 
+  //         fieldList.push(doc.get());
+  //       })
+  //     })
+  //     return fieldList;
+  // }
+  // this.facilityList =  this.fireStoreService.getFieldsFromCollection( this.collectionService.facilityCollection, "Name")
+  // console.log("Das sind die Data im Dialog", this.facilityList);
+  // this.onQuery( this.firestore.collection('users', ref => ref
+  // .where("roles", "==", trainee)));
 }
--- Kack2App/src/app/services/firestore/firestore.service.ts
+++ Kack2App/src/app/services/firestore/firestore.service.ts
@@ -15,19 +15,19 @@ export class FirestoreService {
     public collectionService: CollectionsService) {
   }
 
-  getAllFacilities(collection: string){
+  getAllFacilities(collection: string) {
     return this.afs.collection(collection);
   }
 
-  getFieldsFromCollection(collection: string, field: string):Array<any> {
+  getFieldsFromCollection(collection: string, field: string): Array<any> {
     let fieldList: Array<{}> = [];
     this.afs.collection(collection)
       .get().toPromise().then(snapshot => {
-        snapshot.docs.forEach(doc => { 
+        snapshot.docs.forEach(doc => {
           fieldList.push(doc.get(field));
         })
       })
-      return fieldList;
+    return fieldList;
   }
 
   getUserData = (user: any, subscriptionList?: Subscription[]): Promise<any> => {
@@ -37,19 +37,40 @@ export class FirestoreService {
     })
   }
 
-// Setting up user and facilities
- createDocument(collection: string, objData: any) {
-  this.afs.collection(collection).doc(objData.ID).set(objData).then(res => {
-  }).catch(error => {
-    console.log(error);
-  });
-}
-updateDocument(collection: string, objDatass: any) {
-  this.afs.collection(collection).doc(objDatass.ID).set({objDatass}, {merge: true}).then(res => {
-  }).catch(error => {
-    console.log(error);
-  });
-}
+  // Setting up user and facilities
+  createDocument(collection: string, objData: any) {
+    this.afs.collection(collection).doc(objData.ID).set(objData).then(res => {
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+  updateDocument(collection: string, objData: any) {
+    let obj = Object.assign({}, [objData])
+  //   let elems: any[] = [];
+  // let id  = objData[0].UID;
+  //   objData.forEach((element: any) => {
+  //     elems.push(element);
+  //     console.log("Element: ", element)
+  //     console.log("UID: ", element.UID)
+  //     id = element.UID;
+  //   });
+  //   console.log("IDDDDD: ", id);
+  //    console.log("Elements: ", elems);
+    this.afs.collection(collection).doc(objData.UID).set(objData).then(res => {
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+  // setDocument(collection: string, objData: any) {
+  //   objData.forEach((element: any) => {
+  //     console.log("Element: ", element)
+  //     console.log("UID: ", element.UID)
+  //     this.afs.collection(collection).doc(element.UID).set(element).then(res => {
+  //     }).catch(error => {
+  //       console.log(error);
+  //     });
+  //   });
+  // }
   deleteDocument(data: any, collec: string) {
     return this.afs.collection(collec).doc(data.ID).delete();
   }
@@ -67,11 +88,11 @@ updateDocument(collection: string, objDatass: any) {
       );
   }
 
-  createID(objData?: any){
-    if(objData){
+  createID(objData?: any) {
+    if (objData) {
       return objData.ID = this.afs.createId();
     }
-    else{
+    else {
       return this.afs.createId();
     }
   }
